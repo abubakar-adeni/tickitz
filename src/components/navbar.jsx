d5acb9b5078fa8c6e07f256dfe1d193b2e2a4c95
Abubakar Adeni <abubakaradeni1@gmail.com>
2023-08-10 23:17:38 +0700

    add components navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+export default function navbar() {
+  return (
+    <>
+      <nav id='navbar' className="navbar navbar-expand-lg navbar-light bg-white shadow"
+      style={{height: '80px'}}>
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img 
+             src={require("../assets/Tickitz1.png")}
+             alt="icon-app"/>
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link fs-6 fw-bold ps-5" href="#" >List Movie</a>
+              </li>
+            </ul>
+            <button className="btn btn-outline-primary me-2 Login" type="submit" >Login</button>
+            <button className="btn btn-primary Register" type="submit">Register</button>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
