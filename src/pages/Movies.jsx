db32a2672e7efd53e26a53d16e39d7e69b5f564e
Abubakar Adeni <abubakaradeni1@gmail.com>
2023-08-31 04:24:43 +0700

    fix card movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Navbar from '../components/navbar'
 import '../styles/movies.css'
-import { HiOutlineLocationMarker } from "react-icons/hi"
+import { HiOutlineCalendar, HiChevronDown, HiOutlineLocationMarker, HiRewind } from "react-icons/hi"
 import { useNavigate } from 'react-router-dom'
 import axios from "axios"
-import { useLocation } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { FormatRupiah } from '@arismun/format-rupiah'
+import Footer from "../components/footer";
 
 export default function Movies() {
     const location = useLocation()
@@ -32,29 +34,29 @@ export default function Movies() {
         <>
             <Navbar />
 
-            <section class="container-fluid text-center" style={{ height: '500px' }}>
-                <div class="row justify-content-start">
-                    <div class="col-4 movier" >
+            <section className="container-fluid text-center" style={{ height: '500px' }}>
+                <div className="row justify-content-start">
+                    <div className="col-4 movier" >
                         <div className="movie-image border border-dark-subtle" style={{ width: '40vh', height: '55vh' }}>
                             <img className='movies-buy' src={movies.movies_picture} alt="" />
                         </div>
                     </div>
-                    <div class="col-8 typo" style={{ width: '130vh', height: '60vh' }}>
+                    <div className="col-8 typo" style={{ width: '130vh', height: '60vh' }}>
                         <div className="description-movie">
                             <h1 className='title-movie'>{movies.title}</h1>
                             <p className='genre-movie'>{movies.category}</p>
                             <div className="date-release">
-                                <div class="row row-cols-4 row-movie">
-                                    <div class="col col-6 text-start fw">Release Date
+                                <div className="row row-cols-4 row-movie">
+                                    <div className="col col-6 text-start fw">Release Date
                                         <p className='text-start fe'>{movies.release_date}</p>
                                     </div>
-                                    <div class="col col-6 text-start fw">Duration
+                                    <div className="col col-6 text-start fw">Duration
                                         <p className='text-start fe'>{movies.duration}</p>
                                     </div>
-                                    <div class="col col-6 text-start fw">Directed by
+                                    <div className="col col-6 text-start fw">Directed by
                                         <p className='text-start fe'>{movies.director}</p>
                                     </div>
-                                    <div class="col col-6 text-start fw">Cast
+                                    <div className="col col-6 text-start fw">Cast
                                         <p className='text-start fe'>{movies.cast}</p>
                                     </div>
                                 </div>
@@ -64,68 +66,80 @@ export default function Movies() {
                                 <h6 className='fw-bold'>Synopsis</h6>
                             </div>
                             <p className='synopsis'>
-                            {movies.description}
+                                {movies.description}
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className="showtimes row" style={{ backgroundColor: '#F5F6F8', height: '500px' }}>
+                <div className="showtimes row" style={{ backgroundColor: '#F5F6F8', height: '550px' }}>
                     <h4 className='mt-5 text-center fw-bold'>Showtimes and Tickets</h4>
-                    <div class="row justify-content-evenly">
-                        <div class="col-4">
-                            <div class="dropdown">
-                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <HiOutlineLocationMarker className='me-2' /> Location
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Jakarta</a></li>
-                                    <li><a class="dropdown-item" href="#">Bandung</a></li>
-                                    <li><a class="dropdown-item" href="#">Makassar</a></li>
+                    <di v className="row justify-content-center">
+                        <div className="col-1 me-5">
+                            <div className="dropdown ">
+                                <button className="btn btn-outline-secondary btn-lg  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <HiOutlineCalendar className='me-2' /> dd/mm/yy</button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Jakarta</a></li>
+                                    <li><a className="dropdown-item" href="#">Bandung</a></li>
+                                    <li><a className="dropdown-item" href="#">Makassar</a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="dropdown">
-                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div className="col-1 ms-5">
+                            <div className="dropdown">
+                                <button className="btn btn-outline-secondary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"
+                                >
                                     <HiOutlineLocationMarker className='me-2' /> Location
                                 </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Jakarta</a></li>
-                                    <li><a class="dropdown-item" href="#">Bandung</a></li>
-                                    <li><a class="dropdown-item" href="#">Makassar</a></li>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Jakarta</a></li>
+                                    <li><a className="dropdown-item" href="#">Bandung</a></li>
+                                    <li><a className="dropdown-item" href="#">Makassar</a></li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                    <div class="container text-center">
-                        <div class="row d-lg-flex flex-row justify-content-center align-items-center">
-                            <div class="p-2 mb-2 col-md-4 border border-secondary-subtle" style={{}}>
-                                <img src={require("../assets/Tickitz1.png")}></img>
-                            </div>
-                            <div class="p-2 mb-2 col-md-4 border border-secondary-subtle" style={{}}>
-                                <img src={require("../assets/Tickitz1.png")}></img>
-
-                            </div>
-
-
-                            <div class="card" style={{ width: "18rem" }}>
-                                <img src={require("../assets/Tickitz1.png")} class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to={`/seat/${id}`} class="btn btn-primary">Go somewhere</Link>
+                    </di>
+                    <div className="container text-center">
+                        <div className="row d-lg-flex flex-row justify-content-center align-items-center mt-5">
+                            <div className="p-2 mb-2 col-md-4" style={{ display: 'flex', justifyContent: 'center' }}>
+                                <div className="card justify-content-center align-items-center border border-light-subtle" style={{ width: '20rem', height: '20rem', borderRadius: '15px' }}>
+                                    <div className="card-body">
+                                        <img src={require("../assets/CineOne.png")} />
+                                        <h6 className="card-subtitle mb-2 text-muted mt-1">jl. Rasuna Said No. 12</h6>
+                                        <hr />
+                                        <div class="row mt-3">
+                                            <div class="col-4 mb-4">21:00</div>
+                                        </div>
+                                        <div class="row mt-3">
+                                            <div class="col-4">Price</div>
+                                            <div class="col-8 text-end"><FormatRupiah value={movies.price} /></div>
+                                        </div>
+                                        <Link to={`/seat/${id}`} className="btn book-now mt-5">Book now</Link>
+                                    </div>
                                 </div>
                             </div>
-
-
-
+                            <div className="p-2 mb-2 col-md-4" style={{ display: 'flex', justifyContent: 'center' }}>
+                                <div className="card justify-content-center align-items-center border border-light-subtle" style={{ width: '20rem', height: '20rem', borderRadius: '15px' }}>
+                                    <div className="card-body">
+                                        <img src={require("../assets/CineOne.png")} />
+                                        <h6 className="card-subtitle mb-2 text-muted mt-1">Jl. Sudirman No. 1</h6>
+                                        <hr />
+                                        <div class="row mt-3">
+                                            <div class="col-4 mb-4">21:00</div>
+                                        </div>
+                                        <div class="row mt-3">
+                                            <div class="col-4">Price</div>
+                                            <div class="col-8 text-end"><FormatRupiah value={movies.price} /></div>
+                                        </div>
+                                        <Link to={`/seat/${id}`} className="btn book-now mt-5">Book now</Link>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <Footer />
             </section>
-
-
-
         </>
     )
 }
