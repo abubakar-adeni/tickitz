77084607b090019cfac1e34a8ec6d70d6f6f1a46
Abubakar Adeni <abubakaradeni1@gmail.com>
2023-09-03 22:55:01 +0700

    add documentation

--- src/pages/ticket-result.jsx
+++ src/pages/ticket-result.jsx
@@ -38,8 +38,8 @@ export default function Ticket() {
     return (
         <>
             <Navbar />
-            <div className="container-fluid" style={{ backgroundColor: '#F5F6F8', height: '700px'}}>
-                <div className="container text-center  bg-white mt-5" style={{ borderRadius: '15px', height: '580px' }}>
+            <div className="container-fluid" style={{ backgroundColor: '#F5F6F8', height: '700px' }}>
+                <div className="container text-center  bg-white" style={{ borderRadius: '15px', height: '580px' }}>
                     <div className="row align-items-start">
                         <h1 className='mt-3'>Proof of Payment</h1>
                         <div className="col d-flex justify-content-center">
