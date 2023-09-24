8ce642e6707e62c1c00bd8c55c5fdc2c86c67719
Abubakar Adeni <abubakaradeni1@gmail.com>
2023-09-25 00:56:29 +0700

    fix

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -44,7 +44,7 @@ export default function Movies() {
             {isMobile ? (<>
                 <section class="container-fluid">
                     <div class="d-flex justify-content-center m-5 border border-dark-subtle rounded-2" style={{ height: '50vh' }}>
-                        {isLoading ? (<Skeleton style={{ height: '100%', width: '100%'}} />) : (
+                        {isLoading ? (<Skeleton height={'40vh'} width={'25vh'} />) : (
                             <img className='movies-picture' src={movies.movies_picture} alt="movie-picture" loading="lazy" />
                         )}
                     </div>
